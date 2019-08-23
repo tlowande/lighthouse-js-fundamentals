efe2b151b2193d0a25616daf6ffba0fbd94fee6d
Tamires <tamires.lowande@gmail.com>
2019-08-23 14:43:08 +0000

    adding functions exercise

---
+++ hello.js
@@ -0,0 +1,12 @@
+const sayHellop  = function () {
+  console.log("Hello, world");
+}
+
+sayHellop();
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
