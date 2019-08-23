681ccf12951e2c4c2042bfec2c90214ff3b4d6a4
Tamires <tamires.lowande@gmail.com>
2019-08-23 15:13:06 +0000

    hello.js modified

--- hello.js
+++ hello.js
@@ -10,3 +10,15 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+"\n"
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
